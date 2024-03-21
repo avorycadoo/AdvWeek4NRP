03bead57dbaccbc5d456bb322a338ca9eb2f1b91
avorycadoo <valerinerin24@gmail.com>
2024-03-21 13:38:20 +0700

    valerin - 160421046

---
+++ app/src/main/java/com/example/advweek4nrp/view/StudentDetailFragment.kt
@@ -0,0 +1,46 @@
+package com.example.advweek4nrp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.advweek4nrp.R
+import com.example.advweek4nrp.databinding.FragmentStudentDetailBinding
+import com.example.advweek4nrp.databinding.FragmentStudentListBinding
+import com.example.advweek4nrp.viewmodel.DetailViewModel
+
+class StudentDetailFragment : Fragment() {
+    private lateinit var binding: FragmentStudentDetailBinding
+    private lateinit var detailViewModel: DetailViewModel
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStudentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        detailViewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+
+        // Call fetch function
+        detailViewModel.fetch()
+
+        // Observer for the Student LiveData
+        detailViewModel.studentLD.observe(viewLifecycleOwner, { student ->
+            binding.apply {
+                txtID.setText(student.id)
+                txtName.setText(student.name)
+                txtBod.setText(student.dob)
+                txtPhone.setText(student.phone)
+            }
+        })
+    }
+
+}
